2741cd8a8534537ca2f25a0f7ea163e57efb548b
chrisdeckard2 <christopherdaviddeckard@gmail.com>
2024-06-19 15:24:41 -0400

    Add project files.

---
+++ ExtensionMethods/MyFirstExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods;
+
+public static class MyFirstExtensionMethods {
+
+    public static void Debug(this string str, string label) {
+        Console.WriteLine($"{label} : [{str}]");
+    }
+
+    public static void Debug(this int i, string label) {
+        Console.WriteLine($"{label} : [{i}]");
+    }
+
+    public static int Cubed(this int i) {
+        return i * i * i;
+    }
+
+    public static int Func(this int x) {
+        return (x * x) + (x * 3) + 5;
+
+
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtensionMethods;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        var str = "This is a test.";
+
+        str.Debug("str is");
+
+        var i = 12;
+        i.Debug("i is ");
+
+        i.Cubed().Debug("I cubed");
+
+        i.Func().Debug("(f{i}) = ");
+
+
+
+    }
+}
